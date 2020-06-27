da9c1cb34f71360641881b4b9c877b221afa3fbf
Admin <superpananda@gmail.com>
2020-06-27 22:28:33 +0700

    Добавлен шаблон MVC

---
+++ VueAspNotebook/ClientApp/Home/index.js
@@ -0,0 +1,9 @@
+﻿import Vue from 'vue';
+import HomeComponent from './components/home.vue';
+
+new Vue({
+    el: '#app',
+    components:{
+        HomeComponent
+    }
+})
---
+++ VueAspNotebook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VueAspNotebook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
